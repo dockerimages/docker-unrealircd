6fd5078d157965081447fb4762fc1b42d71bec2f
Frank Lemanschik <frank@github.dspeed.eu>
2015-09-24 11:40:49 +0100

    Update deploy-unrealirc.sh

--- deploy-unrealirc.sh
+++ deploy-unrealirc.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 source /config
-
-wget http://www.unrealircd.com/downloads/Unreal$VERSION.tar.gz
-tar -zxvf Unreal$VERSION.tar.gz
-cd Unreal$VERSION
+cd /tmp
+wget http://www.unrealircd.com/downloads/Unreal$UNREAL_VERSION.tar.gz
+tar -zxvf Unreal$UNREAL_VERSION.tar.gz
+cd /tmp/Unreal$UNREAL_VERSION
 ./Config
 make
-cp /unreal.conf ./unrealircd.conf
+cp /unreal.conf /tmp/Unreal$UNREAL_VERSION/unrealircd.conf
 echo $MOTD > ircd.motd
 echo $RULES > ircd.rules
-./unreal start
+/tmp/Unreal$UNREAL_VERSION/unreal start
+cd /tmp
 wget http://sourceforge.net/projects/anope/files/anope-stable/Anope%20$ANOPE_VERSION/anope-$ANOPE_VERSION-source.tar.gz/download
 # wget http://sourceforge.net/projects/anope/files/anope-stable/Anope%201.8.8/anope-1.8.8.tar.gz/download
 tar -zxvf anope-$ANOPE_VERSION.tar.gz
