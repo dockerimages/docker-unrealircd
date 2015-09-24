a36d3fb7c29aa8b561dedf8114065e65ed073fc3
Frank Lemanschik <frank@github.dspeed.eu>
2015-09-24 11:25:40 +0100

    Update deploy-unrealirc.sh

--- deploy-unrealirc.sh
+++ deploy-unrealirc.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-wget http://www.unrealircd.com/downloads/Unreal3.2.10.2.tar.gz
-tar -zxvf Unreal3.2.10.2.tar.gz
-cd Unreal3.2.10.2
+UNREAL_VERSION="3.2.10.5"
+ANOPE_VERSION="2.0.2"
+MOTD="Message of the Day"
+RULES=""
+
+wget http://www.unrealircd.com/downloads/Unreal$VERSION.tar.gz
+tar -zxvf Unreal$VERSION.tar.gz
+cd Unreal$VERSION
 ./Config
 make
 cp /unreal.conf ./unrealircd.conf
-echo "" > ircd.motd; echo "" > ircd.rules
+echo $MOTD > ircd.motd
+echo $RULES > ircd.rules
 ./unreal start
-wget http://sourceforge.net/projects/anope/files/anope-stable/Anope%201.8.8/anope-1.8.8.tar.gz/download
-tar -zxvf anope-1.8.8.tar.gz
-cd anope-1.8.8
+wget http://sourceforge.net/projects/anope/files/anope-stable/Anope%20$ANOPE_VERSION/anope-$ANOPE_VERSION-source.tar.gz/download
+# wget http://sourceforge.net/projects/anope/files/anope-stable/Anope%201.8.8/anope-1.8.8.tar.gz/download
+tar -zxvf anope-$ANOPE_VERSION.tar.gz
+cd anope-$ANOPE_VERSION
 ./Config
 make
 make install
